07882d6e8948284be7518fffc774d7f81fa07def
traffens <tyler.raffensperger@verizondigitalmedia.com>
2017-07-14 11:39:08 -0400

    Integrated server_comm and directorywatcher. Dwatcher is final file

--- script/directorywatcher.py
+++ script/directorywatcher.py
@@ -1,11 +1,17 @@
+import boto3
+import os
 import os, time
+
+
+s3 = boto3.resource('s3')
+
 path_to_watch = "../xmls"
 before = dict ([(f, None) for f in os.listdir (path_to_watch)])
 while 1:
   time.sleep (10)
   after = dict ([(f, None) for f in os.listdir (path_to_watch)])
   added = [f for f in after if not f in before]
-  removed = [f for f in before if not f in after]
-  if added: print "Added: ", ", ".join (added)
-  if removed: print "Removed: ", ", ".join (removed)
-  before = after
+  if added:
+      print "Added: ", ", ".join (added)
+      data = open("../xmls"+added, 'rb')
+      s3.Bucket("gen3-interns-trigger").put_object(key=added, Body=data)
