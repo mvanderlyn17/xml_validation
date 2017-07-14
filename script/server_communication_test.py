9aba45085f846de7d774836b7e3b8928332450e7
Michael <michael.vanderlyn@verizondigitalmedia.com>
2017-07-14 11:08:38 -0400

    Worked more on server communication

--- script/server_communication_test.py
+++ script/server_communication_test.py
@@ -1,6 +1,8 @@
 import boto3
-
+import os
 # Let's use Amazon S3
 s3 = boto3.resource('s3')
-for bucket in s3.buckets.all():
-    print(bucket.name)
+for file in os.listdir("../media"):
+    print(file)
+    data = open("../media/"+file, 'rb')
+    s3.Bucket('gen3-interns-trigger').put_object(Key=file, Body=data)
