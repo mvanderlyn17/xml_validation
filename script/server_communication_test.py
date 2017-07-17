a6a143cf940735170c4f2c63eb059b8bf9a3a517
Michael <michael.vanderlyn@verizondigitalmedia.com>
2017-07-17 15:18:26 -0400

    Added a file that contains the lambda code to open xmls from our bucket

--- script/server_communication_test.py
+++ script/server_communication_test.py
@@ -2,7 +2,7 @@ import boto3
 import os
 # Let's use Amazon S3
 s3 = boto3.resource('s3')
-for file in os.listdir("../media"):
+for file in os.listdir("../xmls"):
     print(file)
-    data = open("../media/"+file, 'rb')
+    data = open("../xmls/"+file, 'rb')
     s3.Bucket('gen3-interns-trigger').put_object(Key=file, Body=data)
