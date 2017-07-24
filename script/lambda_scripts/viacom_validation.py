3b361a2e3e0e8dcaee7b159a852e3627fbfff34f
traffens <tyler.raffensperger@verizondigitalmedia.com>
2017-07-24 16:19:31 -0400

    once again fixed the viacom validation

--- script/lambda_scripts/viacom_validation.py
+++ script/lambda_scripts/viacom_validation.py
@@ -1,6 +1,4 @@
-from xml.dom import minidom
-from datetime import datetime, date
-xml_file = minidom.parse('C:\\Users\\sshukla\\Desktop\\AWS Lambda\\Paramount.xml')
+#xml_file = minidom.parse('C:\\Users\\sshukla\\Desktop\\AWS Lambda\\Paramount.xml')
 t1=t2=t3=t4=t5=t6=t7=t8=t9=t10=t11=t12=t13=t14=t15=t16=t17=t18=t19=0
 
 package = xml_file.getElementsByTagName('package')
